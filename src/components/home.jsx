c21f5b203b631ad93ef7706fc69e839935a09e30
ridsb <riddhi.bisht2021@vitstudent.ac.in>
2024-03-03 02:42:56 +0530

    first commit

---
+++ src/components/home.jsx
@@ -0,0 +1,18 @@
+
+
+//components
+import Code from './code';
+import Result from './result';
+import Header from './header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+        </>
+    )
+}
+
+export default Home;
